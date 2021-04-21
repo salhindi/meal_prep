499a0df9ade0fd3e4216e1fe7a7fb2cd007808d5
salhindi <calisaraalhindi@gmail.com>
2021-04-20 20:51:08 -0700

    helpers

--- sinatra-meal-prep-app/app/controllers/users_controller.rb
+++ sinatra-meal-prep-app/app/controllers/users_controller.rb
@@ -4,7 +4,22 @@ class UsersController < ApplicationController
         erb :'registrations/login'
     end
 
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+
+        if @user && @user.authenticate(password: params[:password])
+            session[:user_id] = @user.id
+        else
+            redirect "/users/#{@user.id}"
+        end
+    end
+
     get '/signup' do
         erb :'registrations/signup'
     end
+
+    get '/users/:id' do 
+        erb :'meals/show_meals'
+    end
+
 end
