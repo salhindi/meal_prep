3edbbdfa163d04008e6018f9029f974733a6f6d9
salhindi <calisaraalhindi@gmail.com>
2021-04-22 15:35:18 -0700

    edit routes

--- sinatra-meal-prep-app/app/controllers/meals_controller.rb
+++ sinatra-meal-prep-app/app/controllers/meals_controller.rb
@@ -12,25 +12,49 @@ class MealsController < ApplicationController
     post '/meals' do
         if !logged_in? 
             redirect "/"
-        end
-            if params[:name] != "" && params[:ingredients] != "" && params[:meal_time] != ""
-                @meal = Meal.create(name: params[:name], ingredients: params[:ingredients], meal_time: params[:meal_time], user_id: current_user.id)
-                redirect "/meals/#{@meal.id}"
-            else
-                redirect '/meals/new'
             end
-        
+        if params[:name] != "" && params[:ingredients] != "" && params[:meal_time] != ""
+            @meal = Meal.create(name: params[:name], ingredients: params[:ingredients], meal_time: params[:meal_time], user_id: current_user.id)
+            redirect "/meals/#{@meal.id}"
+        else
+            redirect '/meals/new'
+        end
     end
 
     get '/meals/:id' do 
-        @meal = Meal.find(params[:id])
+        set_meal
         erb :'/meals/show_meals'
     end
 
     get '/meals/:id/edit' do 
-        @meal = Meal.find(params[:id])
-        erb :"meals/edit"
+        set_meal
+        if logged_in?
+            if @meal.user == current_user
+                erb :"/meals/edit"
+            else
+                redirect "/users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
     end
 
+    patch '/meals/:id' do 
+        set_meal
+        if logged_in?
+            if @meal.user == current_user
+                @meal.update(name: params[:name], ingredients: params[:ingredients], meal_time: params[:meal_time], photo: params[:photo])
+                redirect "/meals/#{@meal.id}"
+            else
+                redirect "/users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
+    end
 
+    private
+    def set_meal
+        @meal = Meal.find(params[:id])
+    end 
 end
